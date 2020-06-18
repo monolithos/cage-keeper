dcab31ee420fb133ebb387780e269f498a40e47f
captain13128 <capitan.develop@gmail.com>
2020-06-18 12:12:23 +0300

    change run.py

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
+import os
+
 from cage_keeper.cage_keeper import CageKeeper
 
-RPC_HOST = "https://kovan.infura.io/v3/683836c8b9384898a9f99d483ae389bc"
+
+BASE_PATH = os.path.dirname(__file__)
+
 NETWORK = "kovan"
+# NETWORK = "mainnet"
 
-ETH_FROM = "0xC0CCab7430aEc0C30E76e1dA596263C3bdD82932"
-KEY_FILE = "/home/captain/development/dss-deploy-scripts/keystore.json"
-PASS_FILE = "/home/captain/development/dss-deploy-scripts/p.pass"
-
-ADDRESSES_FILE = "/home/captain/development/makerdao_python/chief-keeper/addresses/kovan-addresses.json"
+# ETHEREUM_SETTINGS
+if NETWORK.upper() == "KOVAN":
+    RPC_HOST = 'https://kovan.infura.io/v3/*****'
+    ETH_FROM = '0x0000000000000000000000000000000000000000'
+    KEY_FILE = "/PATH/TO/KEY/FILE.json"
+    PASS_FILE = "/PATH/TO/PASS/FILE.pass"
+    ADDRESSES_FILE = os.path.join(BASE_PATH, 'addresses', 'kovan-addresses.json')  # or '/PATH/TO/ADDRESS/FILE.json'
+elif NETWORK.upper() == "MAINNET":
+    RPC_HOST = 'https://mainnet.infura.io/v3/*****'
+    ETH_FROM = "0x0000000000000000000000000000000000000000"
+    KEY_FILE = "/PATH/TO/KEY/FILE.json"
+    PASS_FILE = "/PATH/TO/PASS/FILE.pass"
+    ADDRESSES_FILE = os.path.join(BASE_PATH, 'addresses', 'mainnet-addresses.json')  # or '/PATH/TO/ADDRESS/FILE.json'
+else:
+    raise Exception('NOT SUPPORTED NETWORK')
 
 
 if __name__ == '__main__':
